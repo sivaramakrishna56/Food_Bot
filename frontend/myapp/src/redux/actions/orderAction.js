9e8db31aaccddba06825dd7d38fd190f7cdd0fc0
Sivaramakrishna Nainaru <140140073+sivaramakrishna56@users.noreply.github.com>
2025-03-12 22:15:43 +0530

    Update orderAction.js

--- frontend/myapp/src/redux/actions/orderAction.js
+++ frontend/myapp/src/redux/actions/orderAction.js
@@ -31,7 +31,7 @@ export const placeOrderAction = (orderData) => async (dispatch) => {
 
         dispatch({ type: "PLACE_ORDER_REQUEST" });
 
-        const { data } = await axios.post("http://localhost:5000/api/orders/place-order", orderData);
+        const { data } = await axios.post("https://food-bot-backend.onrender.com/api/orders/place-order", orderData);
 
         dispatch({ type: "PLACE_ORDER_SUCCESS", payload: data });
     } catch (error) {
